549747db111d901851d4dc8c0501c1561541d80d
Leon Kofman <lkofman1@gmail.com>
2018-09-13 16:51:54 +0300

    nodeJs with connectionPool

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 var express=require('express');
 var mysql=require('mysql')
 var app=express();
+var port=1337
 
 
-var connection=mysql.createConnection({
+var connection=mysql.createPool({
+  connectionLimit:50,
   host: 'localhost',
   user:"springuser",
   password:"ThePassword",
   database:'test'
 });
 
-connection.connect( function(error){
-    if(!!error) {
-      console.log('Error');
-    } else {
-      console.log('Connected');
+console.log("listening  to port "+port );
+app.get('/',function(req,resp){
+  connection.getConnection(function(error,tempCont){
+    if(!!error){
+      tempCont.release();
+      console.log("Error")
+    }else{
+        console.log('Connected');
+        tempCont.query("SELECT * FROM users ",function(error,rows,fields){
+          if(!!error){
+            console.log('Error in  query');
+          }else {
+              resp.json(rows);
+          }
+        })
     }
-});
-
-app.get('/',function(req,res){
-  //about sql
-  connection.query("SELECT * FROM  users",function(error,raws,fields){
-      if(!!error)
-      {
-        console.log('Error in the query');
-      }else{
-        console.log('Success \n');
-        console.log(raws.length)
-        for(i=0;i<raws.length;i++){
-        console.log(raws[i].first_name+"\t "+raws[i].last_name);
-
-      }
-      }
-
-  });
+  })
 })
-app.listen(1337);
+
+
+app.listen(port);
